ea0591aa64505f6c42d28676c4ebffdccb08c081
rekikamed <rekikamed@gmail.com>
2023-10-07 10:41:23 +0100

    add second method

--- maine.py
+++ maine.py
@@ -4,5 +4,9 @@ numbers = [1, 2, 3, 4, 5]
 # Calculate the sum of the numbers using the built-in sum() function
 sum_of_numbers = sum(numbers)
 
+s = 0
+for el in numbers:
+    s = s + el
+print(s)
 # Print the result
 print("The sum of the numbers is:", sum_of_numbers)
